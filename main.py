63d0cc90f9a0cee7a3be2fa68db9294bcd805fd9
GhostKnight183 <sasaalistratov9@gmail.com>
2025-06-15 15:45:45 +0300

    .

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from api import registration_router,login_router
 from core import async_engine
 from models import Base
 from fastapi import FastAPI
-
-
 import uvicorn
 
 app = FastAPI()
@@ -13,7 +11,8 @@ async def startup():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-
+app.include_router(registration_router)
+app.include_router(login_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app",host = "127.0.0.1",port=8000)
